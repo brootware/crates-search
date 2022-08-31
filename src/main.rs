f16aaf984c6edb8bdc7e3624122dd7f28c75331f
rootware <ominbruce@outlook.com>
2022-08-31 09:49:34 +0800

    adding in Crate search implementation with serde json

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,37 @@ struct Args {
     exact: bool,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct Crate {
+    pub name: String,
+    #[serde(rename = "max_version")]
+    pub version: String,
+    pub description: Option<String>,
+    pub license: Option<String>,
+    #[serde(rename = "homepage")]
+    pub homepage_url: Option<String>,
+    #[serde(rename = "repository")]
+    pub repository_url: Option<String>,
+    #[serde(rename = "documentation")]
+    pub documentation_url: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct Crates {
+    crates: Vec<Crate>,
+}
+
+impl Crate {
+    pub fn search(query: &str, limit: f32) -> Result<Vec<Crate>, reqwest::Error> {
+        // ...
+        let url = format!("https://crates.io/api/v1/crates?page=1&per_page={}&q={}",
+                      limit,query);
+        let mut res = reqwest::get(&url)?;
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
-    
+
 }
